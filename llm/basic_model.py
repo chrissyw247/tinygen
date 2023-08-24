9c646329bda3112e01cc58de0a7ea66f1a2ee530
christinewang <christinewang@roblox.com>
2023-08-24 00:57:32 -0700

    set-up reflection placeholder code

--- llm/basic_model.py
+++ llm/basic_model.py
@@ -4,13 +4,14 @@ import openai
 import os
 import json
 
-FILE_HEADING = "##{}##"
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
+# TODO: move these format/parsing methods to separate util
 def format_filename_heading(filename):
-    return FILE_HEADING.format(filename)
+    return f"##{filename}##"
 
 def format_source_code_str(source_code_dict):
+    # TODO: add a new line at top of file?
     source_code_str = "\n".join([f"{format_filename_heading(filename)}:\n{source_code}" for filename, source_code in source_code_dict.items()])
     return source_code_str
 
@@ -45,9 +46,7 @@ def parse_source_code_str(source_code_str, filenames):
 
     return source_code_dict
 
-def generate_code_changes(prompt, source_code_dict):
-    source_code_str = format_source_code_str(source_code_dict)
-    filenames = list(source_code_dict.keys())
+def generate_code_changes(prompt, source_code_str):
     response = {}
 
     try:
@@ -62,6 +61,33 @@ def generate_code_changes(prompt, source_code_dict):
         raise HTTPException(status_code=400, detail="Github repo is too large :(")
 
     generated_code_str = response.choices[0].text.strip()
+    return generated_code_str
+
+def generate_validated_code_changes(prompt, source_code_dict, num_validations=1):
+    # Build source code string (to pass to GPT) from dictionary of files to their contents
+    source_code_str = format_source_code_str(source_code_dict)
+    generated_code_str = generate_code_changes(prompt, source_code_str)
+
+    # Verify edits made by GPT
+    verify_generated_code(prompt, source_code_str, generated_code_str)
+
+    # TODO: actually act on verification response
+    # Parse generated code dictionary from generated code string (returned by GPT)
+    filenames = list(source_code_dict.keys())
     generated_code_dict = parse_source_code_str(generated_code_str, filenames)
-    print(f"GPT generated code: {json.dumps(generated_code_dict, indent=4)}")
     return generated_code_dict
+
+def verify_generated_code(prompt, source_code_str, generated_code_str):
+    print(f"source_code_str: {source_code_str}")
+    print(f"generated_code_str: {generated_code_str}")
+
+    # TODO: actually call GPT to verity
+    # response = openai.ChatCompletion.create(
+    #     model="gpt-3.5-turbo",
+    #     messages = [
+    #         {"role": "system", "content": f"{source_code_str}"},
+    #         {"role": "user", "content": f"${prompt}"},
+    #     ]
+    #     temperature=0,
+    #     max_tokens=1000
+    # )
--- server/generate_operation.py
+++ server/generate_operation.py
@@ -5,7 +5,7 @@ import sys
 sys.path.append('..')
 from utils.github_helper import clone_repo, checkout_branch, commit_changes, get_diff_string
 from utils.file_io_helper import collect_source_code, update_source_code
-from llm.basic_model import format_source_code_str, generate_code_changes
+from llm.basic_model import format_source_code_str, generate_validated_code_changes
 from database.operations import write_to_db
 
 # TODO: serve the backend public/live
@@ -19,7 +19,7 @@ def main(repo_url, prompt):
 
     source_code_dict = collect_source_code("./")
 
-    generated_code_dict = generate_code_changes(prompt, source_code_dict)
+    generated_code_dict = generate_validated_code_changes(prompt, source_code_dict)
     update_source_code(generated_code_dict)
 
     commit_changes("Modified based on prompt")
