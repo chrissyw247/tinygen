199795c2384586dec1f99b867f831771afd4e3c3
christinewang <christinewang@roblox.com>
2023-08-23 23:21:33 -0700

    fix bugs + typos in model parsing

--- llm/basic_model.py
+++ llm/basic_model.py
@@ -11,7 +11,7 @@ def format_filename_heading(filename):
     return FILE_HEADING.format(filename)
 
 def format_source_code_str(source_code_dict):
-    source_code_str = "\n".join([f"{format_filename_heading(filename)}:\n{souce_code}" for filename, souce_code in source_code_dict.items()])
+    source_code_str = "\n".join([f"{format_filename_heading(filename)}:\n{source_code}" for filename, source_code in source_code_dict.items()])
     return source_code_str
 
 def parse_source_code_str(source_code_str, filenames):
@@ -26,12 +26,12 @@ def parse_source_code_str(source_code_str, filenames):
             continue
 
         # The filename and colon length is added to find the starting position of the content
-        start_pos += len(f"{filename}:\n")
+        start_pos += len(f"{format_filename_heading(filename)}:\n")
 
         # Find the next filename to get the ending position of the current file's content
         if filenames.index(filename) < len(filenames) - 1:
             next_filename = filenames[filenames.index(filename) + 1]
-            end_pos = source_code_str.find(f"{next_filename}:", start_pos)
+            end_pos = source_code_str.find(f"{format_filename_heading(next_filename)}:", start_pos)
         else:
             end_pos = len(source_code_str)
 
